24f9056930473b6de31c2aef9fb2de3d3c7f229c
songxiaoyuan <xiaoyuan@iscas.ac.cn>
2017-03-30 11:16:06 +0800

    init the env

---
+++ fatFinger/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+//请求编号
+int requestId=0;
+
+// 前置地址
+char mdFront[]   ="tcp://asp-sim2-front1.financial-trading-platform.com:26213";
+char tradeFront[]="tcp://asp-sim2-front1.financial-trading-platform.com:26205";
+
+#endif // CONFIG_H_INCLUDED
--- fatFinger/main.cpp
+++ fatFinger/main.cpp
@@ -1,9 +1,42 @@
+#include "main.h"
 #include <iostream>
 
 using namespace std;
 
-int main()
+void test_md(void)
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  //初始化UserApi
+  CThostFtdcMdApi* pUserApi=CThostFtdcMdApi::CreateFtdcMdApi();
+  CtpMdSpi* pUserSpi=new CtpMdSpi(pUserApi); //创建回调处理类对象MdSpi
+  pUserApi->RegisterSpi(pUserSpi);			 // 回调对象注入接口类
+  pUserApi->RegisterFront(mdFront);		     // 注册行情前置地址
+  pUserApi->Init();      //接口线程启动, 开始工作
+  ShowMdCommand(pUserSpi,true);
+  pUserApi->Join();      //等待接口线程退出
+  //pUserApi->Release(); //接口对象释放
+}
+
+void test_order(void)
+{
+  //初始化UserApi
+  CThostFtdcTraderApi* pUserApi = CThostFtdcTraderApi::CreateFtdcTraderApi();
+  CtpTraderSpi* pUserSpi = new CtpTraderSpi(pUserApi);
+  pUserApi->RegisterSpi((CThostFtdcTraderSpi*)pUserSpi);			// 注册事件类
+  pUserApi->SubscribePublicTopic(THOST_TERT_RESTART);					// 注册公有流
+  pUserApi->SubscribePrivateTopic(THOST_TERT_RESTART);			  // 注册私有流
+  pUserApi->RegisterFront(tradeFront);							// 注册交易前置地址
+
+  pUserApi->Init();
+  ShowTraderCommand(pUserSpi,true);
+  pUserApi->Join();
+  //pUserApi->Release();
+}
+int main(int argc, const char* argv[]){
+  //g_hEvent=CreateEvent(NULL, true, false, NULL);
+
+  //if(argc < 2)  cerr<<"格式: 命令 参数, 输入有误."<<endl;
+  //else if(strcmp(argv[1],"--md")==0)    test_md();
+  //else if(strcmp(argv[1],"--order")==0) test_order();
+  test_md();
+  return 0;
 }
